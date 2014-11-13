7dc95389328dc3a051f85dd09d9e52278907bcf5
Kouki Saito <dan.addr.skd@gmail.com>
2014-11-13 20:50:29 +0900

    Use affine transform

--- ABCParser/Tone.swift
+++ ABCParser/Tone.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 
 class Tone : MusicalSymbol{
@@ -15,10 +16,38 @@ class Tone : MusicalSymbol{
     var sharp = false
     var natural = false
     var flat = false
+    
     init(tone:String, length:String = "1"){
         super.init()
         self.tone = tone
         self.updateLength(length)
+    }
+    
+    override func render(x: Int, y: Int) {
+        let circle = UIBezierPath(ovalInRect: CGRectMake(CGFloat(x), CGFloat(y), 13, 9))
+        UIColor.blackColor().setFill()
+        
+        let angle = -30.0 * M_PI / 180.0
+        
+        var move = CGAffineTransformMakeTranslation(CGFloat(-x), CGFloat(-y))
+        let rotate = CGAffineTransformMakeRotation(CGFloat(angle))
+        move = CGAffineTransformConcat(move, rotate)
+        
+        let move2 = CGAffineTransformMakeTranslation(CGFloat(x), CGFloat(y))
+        move = CGAffineTransformConcat(move, move2)
+        
+        circle.applyTransform(move)
+        circle.fill()
+        
+        let path = UIBezierPath()
+        //path.moveToPoint(CGPointMake(CGFloat(x+10+4), CGFloat(y-1)))
+        //path.addLineToPoint(CGPointMake(CGFloat(x+10+4), CGFloat(y+30)))
+        path.moveToPoint(CGPointMake(CGFloat(x+2), CGFloat(y-1)))
+        path.addLineToPoint(CGPointMake(CGFloat(x+2), CGFloat(y+30)))
+        UIColor.blackColor().setStroke()
+        path.lineWidth = 1
+        path.stroke()
+        
     }
     
     
